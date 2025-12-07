b5dabd762e36f0ed658bb547a4daa5b3be886100
Confy Marlow <ivanvohmin45@gmail.com>
2025-12-07 10:27:56 +0700

    just change a color

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFoundPage() {
   return (
     <section className="h-[80vh] w-full flex items-center justify-center flex-col gap-3">
         <h1 className="text-2xl tracking-tight font-bold">Страница не найдена</h1>
-        <Link href='/'>На главную</Link>
+        <Link href='/' className="text-sky-600">На главную</Link>
     </section>
   );
 }
