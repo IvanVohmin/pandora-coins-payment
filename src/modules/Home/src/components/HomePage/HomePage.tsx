2fbbae3ec4b32a37b6542859ed04ab63de1e37f3
Confy Marlow <ivanvohmin45@gmail.com>
2026-02-22 11:23:21 +0700

    remove imports

--- src/modules/Home/src/components/HomePage/HomePage.tsx
+++ src/modules/Home/src/components/HomePage/HomePage.tsx
@@ -1,13 +1,5 @@
 "use client";
 import { Input } from "@/components/ui/input";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { HomePageProps, IProduct } from "@/shared/types/types";
 import { useState } from "react";
